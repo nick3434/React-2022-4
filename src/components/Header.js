3666915f895e7ddc250f21441d32d01bfce350a5
nick <nick3434@gmail.com>
2023-10-17 21:43:15 +0800

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = ({ title }) => {
+    return (
+        <header className="py-5 text-white bg-dark text-center">
+            <h1>{title}</h1>
+        </header>
+    )
+}
+
+export default Header;
